d4ee8de408e5328af8b0e965952e52a171eb3ac4
CRISTE0 <escuderocristian65@gmail.com>
2024-06-08 20:45:57 -0500

    :)

--- src/application/Rutas.jsx
+++ src/application/Rutas.jsx
@@ -18,11 +18,8 @@ import { Ventas } from "../pages/Admin/Ventas";
 import { Pedidos } from "../pages/Admin/Pedidos";
 import { Tallas } from "../pages/Admin/Tallas";
 import { Colores } from "../pages/Admin/Colores";
-<<<<<<< HEAD
 import { Carrito } from "../pages/Landing/Carrito";
-=======
 import { Insumos } from "../pages/Admin/Insumos"
->>>>>>> 951e50f7bc6cf27dcfcdbc4a6b0c2f8ce3e38da8
 
 export const Rutas = () => {
   return (
