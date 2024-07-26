5994df3dd4d5552ed8445fb5eccb6cba9f222fd4
AlejoPluma02 <fernandezalejo937@gmail.com>
2024-07-25 23:39:14 -0500

    lejo

--- src/pages/Landing/Login.jsx
+++ src/pages/Landing/Login.jsx
@@ -2,6 +2,192 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Login = () => {
+  
+  const [Contrasenia, setContrasenia] = useState("");
+
+  const [errors, setErrors] = useState({
+    correo: "",
+    contrasenia: "",
+  });
+
+  const show_alerta = (message, type) => {
+    const MySwal = withReactContent(Swal);
+    MySwal.fire({
+      title: message,
+      icon: type,
+      timer: 2000,
+      showConfirmButton: false,
+      timerProgressBar: true,
+      didOpen: () => {
+        // Selecciona la barra de progreso y ajusta su estilo
+        const progressBar = MySwal.getTimerProgressBar();
+        if (progressBar) {
+          progressBar.style.backgroundColor = "black";
+          progressBar.style.height = "6px";
+        }
+      },
+    });
+  };
+
+  // Función para validar el correo electrónico
+const validateCorreo = (value) => {
+  if (!value) {
+    return "Ingresa tu correo electrónico";
+  }
+  if (/\s/.test(value)) {
+    return "El correo electrónico no puede contener espacios";
+  }
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
+    return "Ingresa un correo electrónico válido";
+  }
+  const length = value.length;
+  if (length < 10 || length > 50) {
+    return "El correo debe tener entre 10 y 50 caracteres";
+  }
+  return "";
+};
+
+// Función para validar la contraseña
+const validateContrasenia = (value) => {
+  if (!value) {
+    return "La contraseña es requerida";
+  } else if (value.length < 8 || value.length > 15) {
+    return "La contraseña debe tener entre 8 y 15 caracteres";
+  }
+  return "";
+};
+
+const handleChangeCorreo = (e) => {
+  const value = e.target.value;
+  setCorreo(value); // Actualiza el estado del correo electrónico
+
+  // Valida el correo electrónico y obtiene el mensaje de error
+  const errorMessage = validateCorreo(value);
+
+  // Actualiza el estado de los errores con el mensaje de error correspondiente
+  setErrors((prevState) => ({
+    ...prevState,
+    correo: errorMessage, // Actualiza el error de correo con el mensaje de error obtenido
+  }));
+};
+
+const handleChangeContrasenia = (e) => {
+  setContrasenia(e.target.value); // Actualiza el estado de la contraseña
+  const errorMessage = validateContrasenia(e.target.value);
+  setErrors((prevState) => ({
+    ...prevState,
+    contrasenia: errorMessage, // Actualiza el error de contraseña con el mensaje de error obtenido
+  }));
+};
+
+// Función para renderizar los mensajes de error
+const renderErrorMessage = (errorMessage) => {
+  return errorMessage ? (
+    <div className="invalid-feedback">{errorMessage}</div>
+  ) : null;
+};
+
+const guardarCliente = async (e) => {
+  e.preventDefault(); // Evita que el formulario se recargue
+
+  const cleanedContrasenia = Contrasenia.trim();
+
+  // Validaciones
+  if (!TipoDocumento) {
+    show_alerta("El tipo documento es necesario", "error");
+    return;
+  }
+
+  if (!NroDocumento) {
+    show_alerta("El número de documento es necesario", "error");
+    return;
+  }
+
+  if (!NombreApellido) {
+    show_alerta("El nombre y apellido son necesarios", "error");
+    return;
+  }
+
+  if (!Telefono) {
+    show_alerta("El teléfono es necesario", "error");
+    return;
+  }
+
+  if (!Direccion) {
+    show_alerta("La dirección es necesaria", "error");
+    return;
+  }
+
+  if (!Correo) {
+    show_alerta("El correo es necesario", "error");
+    return;
+  }
+
+  if (!cleanedContrasenia) {
+    show_alerta("La contraseña es requerida", "error");
+    return;
+  }
+
+  try {
+    if (IdCliente) {
+      // Actualizar Cliente
+      await enviarSolicitud("PUT", {
+        IdCliente,
+        TipoDocumento,
+        NroDocumento,
+        NombreApellido: cleanedNombreApellido,
+        Telefono,
+        Direccion: cleanedDireccion,
+        Correo,
+        Contrasenia: cleanedContrasenia,
+      });
+      show_alerta("Cliente actualizado exitosamente", "success");
+    } else {
+      // Crear Cliente
+      await enviarSolicitud("POST", {
+        TipoDocumento,
+        NroDocumento,
+        NombreApellido: cleanedNombreApellido,
+        Telefono,
+        Direccion: cleanedDireccion,
+        Correo,
+        Contrasenia: cleanedContrasenia,
+        Estado: "Activo",
+      });
+      show_alerta("Cliente registrado exitosamente", "success");
+    }
+    
+    // Limpiar los campos del formulario directamente aquí
+    setTipoDocumento("");
+    setNroDocumento("");
+    setNombreApellido("");
+    setTelefono("");
+    setDireccion("");
+    setCorreo("");
+    setContrasenia("");
+    setValcontrasenia("");
+    setErrors({
+      nroDocumento: "",
+      nombreApellido: "",
+      telefono: "",
+      direccion: "",
+      correo: "",
+      contrasenia: "",
+      valcontrasenia: "",
+    });
+  } catch (error) {
+    // Manejo de errores en la solicitud
+    if (error.response) {
+      show_alerta(error.response.data.message, "error");
+    } else if (error.request) {
+      show_alerta("Error en la solicitud", "error");
+    } else {
+      show_alerta("Error desconocido", "error");
+    }
+    console.log(error);
+  }
+};
+
   return (
     <>
       {/* formulario */}
--- src/pages/Landing/RecuperarContraseña.jsx
+++ src/pages/Landing/RecuperarContraseña.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const RecuperarContraseña = () => {
+  const [Correo, setCorreo] = useState("");
   return (
     <>
       {/* formulario */}
@@ -48,8 +49,3 @@ export const RecuperarContraseña = () => {
     </>
   );
 };
-
-
-
-
-
