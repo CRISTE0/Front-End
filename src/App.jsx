7b3053473a9f2791654b77e8328bc41426925b7f
CRISTE0 <escuderocristian65@gmail.com>
2024-08-03 19:04:09 -0500

    siuuuuuu

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import { Rutas } from "./application/Rutas.jsx";
-import { LandingLayout } from "./layouts/LandingLayout.jsx";
 
 function App() {
 
