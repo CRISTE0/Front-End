e4299c7b020a4795c37195d8853f2397058f2d44
Johan1020 <londonojohan02@gmail.com>
2024-09-02 22:05:06 -0500

    compras

--- src/pages/Admin/Compras.jsx
+++ src/pages/Admin/Compras.jsx
@@ -867,6 +867,9 @@ export const Compras = () => {
                       onChange={handleChange}
                       max={new Date().toISOString().split("T")[0]}
                     />
+                    <small>
+                      Selecciona una fecha dentro de los últimos 8 días.
+                    </small>
                     {renderErrorMessage(errors.Fecha)}
                   </div>
                 </div>
